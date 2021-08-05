1b6ff16340cd080955b05f13fc2a5c8ed10942ab
Michael-Serva <83228504+Michael-Serva@users.noreply.github.com>
2021-08-05 21:54:13 +0200

    before hash

--- readme.md
+++ readme.md
@@ -1,2 +1,9 @@
-# creating false data 
+# Readme *maisonphilo*
+
+## creating false data 
 `https://symfonycasts.com/screencast/symfony4-doctrine/faker-fixtures` / `https://github.com/fzaninotto/Faker`
+`php bin/console doctrine:fixtures:load`
+
+## Random User generator / avatar 
+`https://randomuser.me/`
+`https://randomuser.me/api/portraits/men/1.jpg` <!-- images range from 0 to 100 for men or woman -->
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,19 +6,35 @@ use Faker\Factory;
 use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+
+public function __construct(UserPasswordHasherInterface $encoder)
+{
+}
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('fr-Fr');
+        $users = [];
+        $genres = ['male', 'female'];
         for ($i=1; $i < 30; $i++) { 
+            /* creation of a new user */
             $user = new User();
+            /* creation of a random genre */
+            $genre = $faker->randomElement($genres);
+            $picture = 'https://randomuser.me/api/portraits/men/1.jpg';
+            $pictureId = $faker->numberBetween(1,99).'.jpg';
+            $picture .= ($genre == 'male' ? 'men/' : 'female/'); //$picture = $picture.($genre == 'male' ? 'men/' : 'female/');
             $user->setEmail($faker->email)
             ->setfirstName($faker->firstName)
             ->setLastName($faker->lastName)
-            ->setHash("95610b405A!");
+            ->setHash("95610b405A!")
+            ->setPicture($picture)
+            ->setGenre($genre);
             $manager->persist($user);
+            $users[] = $user;
         }
         $manager->flush();
     }
